7179acb40308fa15ff578def1b4da486112ce86d
Lin Ma <lin.ma@autodesk.com>
2016-11-26 13:23:32 +0800

    Added constructor for component

--- base/ref.h
+++ base/ref.h
@@ -1,17 +1,29 @@
 #ifndef COMMON_REF_H
 #define COMMON_REF_H
 
+/// Ref` is a base class for an object whose lifetime is managed by a ref.
+
+/// See \ref topic_refs for details. This class is often used in 
+/// conjunction with `RefPtr`. When the ref count on the 
+/// object reaches under zero, it will be deleted.
 class Ref
 {
-public:
+    // We don't want those guys....
+    // ... unless we
+    // a. find a way of copying interfaces...
+    // b. handle refs
+    Ref(const Ref& src);
+    Ref& operator=(const Ref& rhs);
+
+protected:
     Ref();
+    virtual ~Ref() = 0;
+
+public:
 	void retain();
 	void release();
 	
 	unsigned int getReferenceCount();
-
-protected:
-	virtual ~Ref();
 
 private:
 	unsigned int _reference_count;
