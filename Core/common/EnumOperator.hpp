9d83891f5842b5340f9e54420833d21a04a20bf5
fanxiaochen <fan.daybreak@gmail.com>
2016-12-16 17:03:35 +0800

    using macros to enable enum operator

--- Core/common/EnumOperator.hpp
+++ Core/common/EnumOperator.hpp
@@ -1,30 +1,18 @@
 #ifndef COMMON_ENUM_OPERATOR_HPP
 #define COMMON_ENUM_OPERATOR_HPP
 
-namespace te
-{
-    template<class T>                                  
-    inline T operator|(T a, T b)                        
-    {                                                   
-        using utype = std::underlying_type<T>::type;    
-        return T(utype(a) | utype(b));                  
-    }                                                   
-
-    template<class T>                                   
-    inline T operator&(T a, T b)                        
-    {                                                   
-        using utype = std::underlying_type<T>::type;    
-        return T(utype(a) & utype(b));                  
-    }
-    template<class T>                                   
-    inline T operator^(T a, T b) 
-    { 
-        using utype = std::underlying_type<T>::type;
-        return T(utype(a) ^ utype(b));
-    }
+#define ENABLE_ENUM_OPERATOR_IN(T,INT_T) \
+namespace te{                            \
+enum class T;                            \
+inline T    operator    &   (T x, T y)      {   return static_cast<T>   (   static_cast<INT_T>(x) & static_cast<INT_T>(y));    }; \
+inline T    operator    |   (T x, T y)      {   return static_cast<T>   (   static_cast<INT_T>(x) | static_cast<INT_T>(y));    }; \
+inline T    operator    ^   (T x, T y)      {   return static_cast<T>   (   static_cast<INT_T>(x) ^ static_cast<INT_T>(y));    }; \
+inline T    operator    ~   (T x)           {   return static_cast<T>   (   ~static_cast<INT_T>(x));                           }; \
+inline T&   operator    &=  (T& x, T y)     {   x = x & y;  return x;   }; \
+inline T&   operator    |=  (T& x, T y)     {   x = x | y;  return x;   }; \
+inline T&   operator    ^=  (T& x, T y)     {   x = x ^ y;  return x;   }; \
 }
-               
 
-
+#define ENABLE_ENUM_OPERATOR(T) ENABLE_ENUM_OPERATOR_IN(T, int)
 
 #endif // COMMON_ENUM_OPERATOR_HPP
