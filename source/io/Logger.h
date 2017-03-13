fd9b40d131b314c5ae727a6050e4e66fc8966728
fanxiaochen <fan.daybreak@gmail.com>
2017-03-13 18:55:50 +0800

    add simple logger

---
+++ source/io/Logger.h
@@ -0,0 +1,52 @@
+#ifndef IO_LOGGER_H
+#define IO_LOGGER_H
+
+#include <ostream>
+#include <fstream>
+#include <string>
+
+#include "math/Vector3.h"
+#include "math/Matrix.h"
+
+#define cLog *(te::ConsoleLogger::getInstance())
+#define fLog *(te::FileLogger::getInstance())
+
+namespace te
+{
+    std::string format(const char* format, ...);
+
+    class FileLogger
+    {
+    public:
+        static FileLogger* getInstance();
+    public:
+        virtual FileLogger& operator<<(const std::string& msg);
+        virtual FileLogger& operator<<(const Vector3& v);
+        virtual FileLogger& operator<<(const Matrix& m);
+    private:
+        FileLogger() = default;
+    private:
+        static FileLogger* _logger;
+
+        std::ofstream   _fout;
+    };
+
+    class ConsoleLogger
+    {
+    public:
+        static ConsoleLogger* getInstance();
+    public:
+        virtual ConsoleLogger& operator<<(const std::string& msg);
+        virtual ConsoleLogger& operator<<(const Vector3& v);
+        virtual ConsoleLogger& operator<<(const Matrix& m);
+    private:
+        ConsoleLogger();
+    private:
+        static ConsoleLogger* _logger;
+
+        std::ostream   _cout;
+    };
+
+}
+
+#endif // IO_LOGGER_H
---
+++ source/test/TestTransform.h
@@ -0,0 +1,12 @@
+
+#include "io/Logger.h"
+#include "math/Vector3.h"
+
+int main()
+{
+    cLog << "fanxiaochen";
+
+    te::Vector3 v(1, 2, 3);
+    cLog << v;
+    return 0;
+}
