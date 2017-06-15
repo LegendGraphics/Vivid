3b8c7925ff08c8818ea870cebfb56de8319cc1d6
fanxiaochen <fan.daybreak@gmail.com>
2017-06-15 18:15:06 +0800

    Add basic light component

---
+++ source/common/Light.h
@@ -0,0 +1,79 @@
+#ifndef COMMON_LIGHT_H
+#define COMMON_LIGHT_H
+
+#include "common/Component.h"
+#include "math/Vector4.h"
+
+namespace te
+{
+    struct Color
+    {
+        float r;
+        float g;
+        float b;
+        float a;
+
+        Color() {};
+        Color(const Color& v) { *this = v; }
+        Color(const Vector4& vec) { *this = vec; }
+        Color(float r, float g, float b, float a = 1.0f) { this->r = r; this->g = g; this->b = b; this->a = a; }
+        Color& operator=(const Color& v)
+        {
+            this->r = v.r; this->g = v.g; this->b = v.b; this->a = v.a;
+            *this;
+        }
+        Color& operator=(const Vector4& vec)
+        {
+            this->r = vec.x; this->g = vec.y; this->b = vec.z; this->a = vec.w;
+            *this;
+        }
+        ~Color() {};
+
+        inline const Color operator+(const Color& v) const { return Color(r + v.r, g + v.g, b + v.b, a + v.a); }
+        inline Color& operator+=(const Color& v) { this->r += v.r; this->g += v.g; this->b += v.b; this->a += v.a; return *this; }
+        inline const Color operator-(const Color& v) const { return Color(r - v.r, g - v.g, b - v.b, a - v.a); }
+        inline Color& operator-=(const Color& v) { this->r -= v.r; this->g -= v.g; this->b -= v.b; this->a -= v.a; return *this; }
+        inline const Color operator*(float s) const { return Color(r * s, g * s, b * s, a * s); }
+        inline Color& operator*=(float s) { this->r *= s; this->g *= s; this->b *= s; this->a *= s; return *this; }
+        inline const Color operator/(float s) const { return Color(r / s, g / s, b / s, a / s); }
+        inline Color& operator/=(float s) { this->r /= s; this->g /= s; this->b /= s; this->a /= s; return *this; }
+
+
+        static Color black;
+        static Color white;
+        static Color red;
+        static Color green;
+        static Color blue;
+    };
+
+    Color Color::black = Color(0, 0, 0, 1);
+    Color Color::white = Color(1, 1, 1, 1);
+    Color Color::red = Color(1, 0, 0, 1);
+    Color Color::green = Color(0, 1, 0, 1);
+    Color Color::blue = Color(0, 0, 1, 1);
+
+
+    enum class LightType
+    {
+        SPOT,
+        DIRECTIONAL,
+        POINT,
+        AREA
+    };
+
+
+    class Light : public ComData
+    {
+    protected:
+        int         _area_size;
+        Color       _color;
+        float       _range;
+        float       _intensity;
+        LightType   _type;
+
+    };
+}
+
+
+#endif // COMMON_LIGHT_H
+
